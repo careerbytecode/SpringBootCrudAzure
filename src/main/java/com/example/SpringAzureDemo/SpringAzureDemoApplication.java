85f2dc4ea71230ff5a3e3ba8c316a02d40931d36
sushmitha <sushmithaachar96@gmail.com>
2025-04-09 02:47:53 +0530

    request mapping is added

--- src/main/java/com/example/SpringAzureDemo/SpringAzureDemoApplication.java
+++ src/main/java/com/example/SpringAzureDemo/SpringAzureDemoApplication.java
@@ -3,13 +3,14 @@ package com.example.SpringAzureDemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 public class SpringAzureDemoApplication {
 
-	@GetMapping("/message")
+	@RequestMapping("/message")
 	public String message() {
 		return "congrats ! you app deployed successfully in Azure";
 	}
